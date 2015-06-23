70849e91a484c9e87111723d29ef2f7bd27cd752
PabloSuarez <hostsuarezpablo@gmail.com>
2015-06-23 02:10:55 -0300

    angular

--- angularDemo/listadetareas.js
+++
@@ -1,5 +0,0 @@
-function ControladorTareas ($scope) {
-	$scope.tareas = [{texto: 'Ser Super heroes con angular js', hecho: true}, 
-					 {texto: 'Crear una aplicacion con angular', hecho: true}];
-
-}
