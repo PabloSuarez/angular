ef65b575564af7f42e225b82be5447a40bc524e1
PabloSuarez <hostsuarezpablo@gmail.com>
2015-06-27 03:36:06 -0300

    angular

--- github_repos/js/controllers.js
+++ github_repos/js/controllers.js
@@ -1,9 +1,36 @@
 angular.module("FinalApp")
-.controller("MainController",function($scope, $resource){
-	Post = $resource('http://jsonplaceholder.typicode.com/posts/:id', {id: '@id'});
+.controller("MainController",function($scope, $resource, PostResource){
 	User = $resource('http://jsonplaceholder.typicode.com/users/:id', {id: '@id'});
 
-	$scope.posts = Post.query();
+	$scope.posts = PostResource.query();
 	$scope.users = User.query();
 
+	$scope.removePost = function(post){
+		PostResource.delete({id: post.id});
+		// como es una demo de api no borra el post
+		// así que lo quitamos manualmente luego
+		$scope.posts = $scope.posts.filter(function(element){
+			return element.id !== post.id;
+		});
+	}
+})
+
+.controller("PostController",function($scope, PostResource, $routeParams){
+	$scope.title = "Editar Post";
+	$scope.post = PostResource.get({id: $routeParams.id});
+	$scope.savePost = function(){
+		PostResource.update({id: $scope.post.id}, {data: $scope.post}, function(data){
+			console.log(data);
+		});
+	}
+})
+
+.controller("NewPostController",function($scope, PostResource){
+	$scope.post = {};
+	$scope.title = "Crear Post";
+	$scope.savePost = function(){
+		PostResource.save({data: $scope.post}, function(data){
+			console.log(data);
+		});
+	}
 });
